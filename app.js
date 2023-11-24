55dc0971ad04fcd5c4bd4230b7026fa45e9500ba
vmdeom <vitormourao.dev@outlook.com>
2023-11-24 17:00:42 -0300

    initial auth setup

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const LocalStorage = require('node-localstorage').LocalStorage;
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs')
 
 dotenv.config({ path: './.env'});
-
-if(typeof localStorage === 'undefined' || localStorage === null){
-    localStorage = new LocalStorage('./scratch');
-}
-else{
-    console.log("has storage");
-}
-
-var isAuth = false;
 
 const port = process.env.PORT;
 
 app = express();
 
+let userState = 'Please, login or register.'
+
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cookieParser());
+
+let data = {logged: 'no'};
 
 app.get('/', function(req, res){
-    res.render("page", {pageToRender: 'userpage.ejs'});
+    res.render("page", {pageToRender: 'userpage.ejs',
+                        user: userState});
 });
 
 app.get('/register', function(req, res){
-    res.render("page", {pageToRender: 'register.ejs'});
+    res.render("page", {pageToRender: 'register.ejs',
+                        message: ''});
 });
 
 app.get('/login', function(req, res){
@@ -35,13 +34,16 @@ app.get('/login', function(req, res){
 });
 
 app.post('/auth/register', function(req, res){
+    const {name, email, password, password_confirm} = req.body
     console.log(req.body);
 });
 
 app.post('/auth/login', function(req, res){
     console.log(req.body);
+    res.cookie('userData', data);
+    res.redirect('/')
 });
 
-app.listen(port || 3000, 'localhost', function(){
+app.listen(port || 3000, '192.168.0.10', function(){
     console.log('listening at port ' + port)
 });
